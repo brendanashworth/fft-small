f6eb359ca02663c3f058bb1fae1627b0c60e6f57
Brendan Ashworth <brendan.ashworth@me.com>
2019-06-11 23:04:20 -0700

    implement fft cooley-tukey
    implementation follows wikipedia psuedocode

--- fft.c
+++ fft.c
@@ -6,9 +6,9 @@
 #include <math.h>
 
 void fft_slow(int* x, double complex* X, int N) {
-    int n, k;
+    uint16_t n, k;
 
-    // Iterate through k = N times.
+    // Iterate through, allowing X_K = sum_N of the complex frequencies.
     for (k = 0; k < N; k++) {
         for (n = 0; n < N; n++) {
             X[k] += x[n] * cexp(-2 * M_PI * I * n * k / N);
@@ -16,6 +16,27 @@ void fft_slow(int* x, double complex* X, int N) {
     }
 }
 
+void fft_radix2(int* x, double complex* X, int N, int s) {
+    uint16_t k;
+    double complex t;
+
+    // At the lowest level pass through (delta T=0 means no phase).
+    if (N == 1) {
+        X[0] = x[0];
+        return;
+    }
+
+    // Cooley-Tukey: recursively split in two, then combine beneath.
+    fft_radix2(x, X, N/2, 2*s);
+    fft_radix2(x+s, X + N/2, N/2, 2*s);
+
+    for (k = 0; k < N/2; k++) {
+        t = X[k];
+        X[k] = t + cexp(-2 * M_PI * I * k / N) * X[k + N/2];
+        X[k + N/2] = t - cexp(-2 * M_PI * I * k / N) * X[k + N/2];
+    }
+}
+
 void fft(int* x, double complex* X, int N) {
-    fft_slow(x, X, N);
+    fft_radix2(x, X, N, 1);
 }
--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 #include <math.h>
 
 #define eps 0.000001
-#define ASSERT_EQ(a, b) if (cabs((a) - (b)) > eps) goto fail;
+#define ASSERT_EQ(c, a, b) if (cabs((a) - (b)) > eps) { printf("FAILURE(%s) %.2f %+.2fi != %.2f %+.2fi\n", (c), creal((a)), cimag((a)), creal((b)), cimag((b))); goto fail; }
 
 int main(int argc, char* argv[]) {
     /* do some simple tests */
@@ -15,22 +15,32 @@ int main(int argc, char* argv[]) {
 
     fft(x, X, 2);
 
-    ASSERT_EQ(X[0], 1 + 0 * I);
-    ASSERT_EQ(X[1], 1 + 0 * I);
+    ASSERT_EQ("X[0]", X[0], 1 + 0 * I);
+    ASSERT_EQ("X[1]", X[1], 1 + 0 * I);
 
     int b[8] = {0, 1, 0, 0, 0, 0, 0, 0};
     double complex B[8];
 
     fft(b, B, 8);
 
-    ASSERT_EQ(B[0], 1 + 0 * I);
-    ASSERT_EQ(B[1], sqrt(2) / 2 - sqrt(2) * I / 2)
-    ASSERT_EQ(B[2], 0 - 1 * I)
-    ASSERT_EQ(B[3], - sqrt(2) / 2 - sqrt(2) * I / 2)
-    ASSERT_EQ(B[4], - 1 + 0 * I)
-    ASSERT_EQ(B[5], - sqrt(2) / 2 + sqrt(2) * I / 2)
-    ASSERT_EQ(B[6], 0 + 1 * I)
-    ASSERT_EQ(B[7], sqrt(2) / 2 + sqrt(2) * I / 2)
+    ASSERT_EQ("B[0]", B[0], 1 + 0 * I);
+    ASSERT_EQ("B[1]", B[1], sqrt(2) / 2 - sqrt(2) * I / 2)
+    ASSERT_EQ("B[2]", B[2], 0 - 1 * I)
+    ASSERT_EQ("B[3]", B[3], - sqrt(2) / 2 - sqrt(2) * I / 2)
+    ASSERT_EQ("B[4]", B[4], - 1 + 0 * I)
+    ASSERT_EQ("B[5]", B[5], - sqrt(2) / 2 + sqrt(2) * I / 2)
+    ASSERT_EQ("B[6]", B[6], 0 + 1 * I)
+    ASSERT_EQ("B[7]", B[7], sqrt(2) / 2 + sqrt(2) * I / 2)
+
+    int c[4] = {1, 3, 5, 7};
+    double complex C[4];
+
+    fft(c, C, 4);
+
+    ASSERT_EQ("C[0]", C[0], 16);
+    ASSERT_EQ("C[1]", C[1], -4 + 4*I);
+    ASSERT_EQ("C[2]", C[2], -4);
+    ASSERT_EQ("C[3]", C[3], -4 - 4*I);
 
     printf("all tests pass\n");
     return 0;
